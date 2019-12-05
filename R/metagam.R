d3017942e94daba1b301e414c15111c82ad6108a
Øystein Sørensen <oyss@medisin.uio.no>
2019-12-05 12:00:14 +0100

    Separated out interpolation in function

--- R/metagam.R
+++ R/metagam.R
@@ -28,32 +28,10 @@ metagam <- function(models, grid_length = 40, probs = c(.01, .025, .05, .95, .97
 
     # Grids for each model
     grid_list <- create_common_grid(x, models, x_comp, x_class, grid_length)
-    x_grids <- grid_list$x_grids
-    common_x_grid <- grid_list$common_x_grid
+
 
     # Interpolate posterior over grid
-    posterior_samples <- Map(function(m, g, i){
-      # Extract the posterior samples at the grid
-      posterior_sample <- m$posterior_sample[g$rows_comp, , drop = FALSE]
-      # Interpolate each posterior curve
-      if(x_class == "numeric"){
-        posterior_sample <- lapply(as.data.frame(posterior_sample), function(y){
-          stats::approx(x = as.numeric(g$x_grid[, x]), y = as.numeric(y), xout = common_x_grid)$y
-        })
-        posterior_sample <- unlist(posterior_sample, use.names = FALSE)
-      } else {
-        posterior_sample <- c(posterior_sample)
-      }
-
-      posterior_sample <- as.data.frame(cbind(common_x_grid, posterior_sample))
-      colnames(posterior_sample) <- c(x, "value")
-      posterior_sample$sample <- i
-
-      return(posterior_sample)
-    }, models, x_grids, seq(from = 1, to = length(models), by = 1))
-
-
-    posterior_samples <- do.call(rbind, posterior_samples)
+    posterior_samples <- interpolate(models, grid_list, x, x_class)
 
     # Compute the posterior quantities of interest
     posterior_samples <- split(posterior_samples, posterior_samples[, x])
