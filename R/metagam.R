b5c578e305f1fec61d650cabb2263d950380298b
Øystein Sørensen <oyss@medisin.uio.no>
2020-01-11 14:01:45 +0100

    Added intercept option

--- R/metagam.R
+++ R/metagam.R
@@ -7,12 +7,14 @@
 #' @param terms Character vector of terms, smooth or parametric.
 #' @param restrict_max char
 #' @param restrict_min char
+#' @param intercept logical defining whether or not to shift the output values
+#'   by the intercept. Only applies when \code{type = "terms"} or \code{type = "iterms"}.
 #'
 #' @return An object of type metagam.
 #' @export
 #'
 metagam <- function(fits, grid, type = "terms", terms = NULL, method = "fixed",
-                    restrict_max = NULL, restrict_min = NULL){
+                    restrict_max = NULL, restrict_min = NULL, intercept = TRUE){
   fit_comb <- purrr::map_dfr(fits, function(fit){
     # Reduce grid to be within the range
     if(!is.null(restrict_min)){
@@ -38,19 +40,23 @@ metagam <- function(fits, grid, type = "terms", terms = NULL, method = "fixed",
       colnames(pred$fit) <- paste0("fit.", colnames(pred$fit))
       colnames(pred$se.fit) <- paste0("se.", colnames(pred$se.fit))
       dat <- dplyr::bind_cols(grid, dplyr::as_tibble(pred$fit), dplyr::as_tibble(pred$se.fit))
-      tidyr::pivot_longer(dat,
-                          cols = union(dplyr::starts_with("fit."), dplyr::starts_with("se.")),
-                          names_to = c(".value", "term"),
-                          names_pattern = "([[:alnum:]]+)\\.([[:alnum:]\\(\\)0-9]+)"
+      dat <- tidyr::pivot_longer(dat,
+                                 cols = union(dplyr::starts_with("fit."), dplyr::starts_with("se.")),
+                                 names_to = c(".value", "term"),
+                                 names_pattern = "([[:alnum:]]+)\\.([[:alnum:]\\(\\)0-9]+)"
       )
+
+      if(intercept){
+        dat <- dplyr::mutate(dat, fit = .data$fit + attr(pred, "constant"))
+      }
+      return(dat)
+
     } else {
       dplyr::mutate(grid, fit = as.numeric(pred$fit),
                     se = as.numeric(pred$se),
                     term = type)
     }
 
-    # Deal with intercept later
-    #attr(pred, "constant")
   }, .id = "cohort")
 
   fit_meta <- dplyr::group_by_at(fit_comb, dplyr::vars(-"fit", -"se", -"cohort"))
