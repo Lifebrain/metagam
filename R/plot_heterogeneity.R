535644e8018f1d6ade38104cd5d78afa811670c3
Brandmaier <brandmaier@mpib-berlin.mpg.de>
2020-01-28 10:52:07 +0100

    added Q+CI plot

--- R/plot_heterogeneity.R
+++ R/plot_heterogeneity.R
@@ -1,5 +1,7 @@
-plot_heterogeneity <- function(x, axis, ...)
+plot_heterogeneity <- function(x, axis, type="Q", ...)
 {
+  
+  if (type=="p") {
   
   gp <- ggplot2::ggplot(data=x$prediction,
                 ggplot2::aes_string(x=axis,y="qp"))+
@@ -7,6 +9,25 @@ plot_heterogeneity <- function(x, axis, ...)
     ggplot2::geom_hline(yintercept=0.05,lty=2)+
     ggplot2::theme_minimal()+
     ggplot2::ylab("Heterogeneity (p)")
+  
+  } else if (type=="Q") {
+    
+    # TODO: øystein, please check whether this is the correct approximation
+    x$prediction$z = -0.862 + sqrt(0.743 - 2.404*log(x$prediction$qp))
+    x$prediction$se = x$prediction$q/x$prediction$z
+  
+  gp <- ggplot2::ggplot(data=x$prediction,
+                        ggplot2::aes_string(x=axis,y="q"))+
+    ggplot2::geom_ribbon(mapping=aes(x=x2,ymin=q-1.96*se,ymax=q+1.96*se),
+                         fill=viridis::viridis(4)[3],
+                         col=viridis::viridis(4)[1])+
+    ggplot2::geom_line()+
+    ggplot2::theme_minimal()+
+    ggplot2::ylab("Heterogeneity (Q)")
+  
+  } else {
+    stop("Unknown type. Try 'Q' or 'p'.")
+  }
   
   return(gp)
 }
