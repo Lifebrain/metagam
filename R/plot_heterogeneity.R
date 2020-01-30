84571658f869cda6f2cb4c79b8d5be9ebb0454ba
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-01-30 16:28:28 +0100

    rearrange heterogeneity

--- R/plot_heterogeneity.R
+++ R/plot_heterogeneity.R
@@ -9,8 +9,8 @@
 #' was fitted with a single term, this one is taken.
 #' @param type Character specifying which type of plot. Either \code{"Q"} for the test statistic
 #' or \code{"p"} for the p-value. Defaults to \code{"Q"}.
-#' @param alpha Significance level. Defaults to \code{.05}.
-#' @param ... Other arguments to plot.
+#' @param alpha_thresh Significance level. Defaults to \code{.05}.
+#' @param ... Other arguments to ggplot.
 #'
 #' @return A ggplot object.
 #' @export
@@ -23,12 +23,33 @@
 #' # vignette("heterogeneity")
 #'
 #'
-plot_heterogeneity <- function(x, axis = NULL, term = NULL, type = "Q", alpha = .05, ...)
+plot_heterogeneity <- function(x, axis = x$xvars, term = NULL, type = "Q", alpha_thresh = .05, ...)
 {
 
-  if(is.null(axis)){
-    axis <- x$xvars
-  }
+  type <- match.arg(type, c("p", "Q"))
+
+  dat <- make_heterogeneity_data(x, axis = axis, term = term)
+
+  gp <- switch(type,
+         "p" = plot_heterogeneity_p(dat, axis, alpha_thresh, ...),
+         "Q" = plot_heterogeneity_q(dat, axis, alpha_thresh, ...)
+         )
+
+  return(gp)
+}
+
+
+# Internals ----
+#' Prepare heterogeneity data
+#'
+#' Internal function to prepare
+#' data for heterogeneity plots
+#'
+#' @inheritParams plot_heterogeneity
+#'
+#' @return tibble/data.frame
+make_heterogeneity_data <- function(x, axis = x$xvars, term = NULL)
+{
 
   if(is.null(term)){
     if(x$type == "iterms"){
@@ -45,40 +66,55 @@ plot_heterogeneity <- function(x, axis = NULL, term = NULL, type = "Q", alpha = 
                        QEp = purrr::map_dbl(.data$meta_model, ~ .$QEp))
   dat <- dplyr::rename_at(dat, dplyr::vars(axis), ~ "x")
 
+  return(dat)
+}
 
-    if (type=="p") {
+#' Heterogeneity p-plot
+#'
+#' Internal function to plot
+#' the heterogeneity p values
+#'
+#' @param data data made by \code{\link{make_heterogeneity_data}}.
+#' @inheritParams plot_heterogeneity
+#'
+#' @return ggproto object
+plot_heterogeneity_p <- function(data, axis, alpha_thresh, ...){
+  ggplot2::ggplot(data = data,
+                  ggplot2::aes(x = .data$x, y = .data$QEp)) +
+    ggplot2::geom_line(...) +
+    ggplot2::geom_hline(yintercept = alpha_thresh, lty = 2) +
+    ggplot2::scale_y_continuous(trans = 'log2') +
+    ggplot2::theme_minimal() +
+    ggplot2::labs(y = "Heterogeneity (p)",
+                  x = axis)
+}
 
-      gp <- ggplot2::ggplot(data = dat, ggplot2::aes(x = .data$x, y = .data$QEp)) +
-        ggplot2::geom_line() +
-        ggplot2::geom_hline(yintercept = alpha, lty = 2) +
-        ggplot2::scale_y_continuous(trans = 'log2') +
-        ggplot2::theme_minimal() +
-        ggplot2::ylab("Heterogeneity (p)") +
-        ggplot2::xlab(axis)
+#' Heterogeneity Q-plot
+#'
+#' Internal function to plot
+#' the heterogeneity Q values
+#'
+#' @param data data made by \code{\link{make_heterogeneity_data}}.
+#' @inheritParams plot_heterogeneity
+#'
+#' @return ggproto object
+plot_heterogeneity_q <- function(data, axis, alpha_thresh, ...){
+  # TODO: øystein, please check whether this is the correct approximation
 
-  } else if (type=="Q") {
+  data <- dplyr::mutate(data,
+                        z = -0.862 + sqrt(0.743 - 2.404 * log(.data$QEp)),
+                        Qse = .data$QE / .data$z
+  )
 
-    # TODO: øystein, please check whether this is the correct approximation
-
-    dat <- dplyr::mutate(dat,
-                  z = -0.862 + sqrt(0.743 - 2.404 * log(.data$QEp)),
-                  Qse = .data$QE / .data$z)
-
-    gp <- ggplot2::ggplot(data = dat, ggplot2::aes(x = .data$x, y = .data$QE)) +
-      ggplot2::geom_ribbon(
-        mapping = ggplot2::aes(ymin = .data$QE + stats::qnorm(!!alpha / 2) * .data$Qse,
-                               ymax = .data$QE + stats::qnorm(1 - !!alpha / 2) * .data$Qse),
-        fill = viridis::viridis(4)[3],
-        col = viridis::viridis(4)[1]
-        ) +
-      ggplot2::geom_line() +
-      ggplot2::theme_minimal() +
-      ggplot2::ylab("Heterogeneity (Q)") +
-      ggplot2::xlab(axis)
-
-  } else {
-    stop("Unknown type. Try 'Q' or 'p'.")
-  }
-
-  return(gp)
+  ggplot2::ggplot(data = data,
+                  ggplot2::aes(x = .data$x, y = .data$QE)) +
+    ggplot2::geom_ribbon(...,
+      mapping = ggplot2::aes(
+        ymin = .data$QE + stats::qnorm(!!alpha_thresh / 2) * .data$Qse,
+        ymax = .data$QE + stats::qnorm(1 - !!alpha_thresh / 2) * .data$Qse)
+    ) +
+    ggplot2::geom_line(...) +
+    ggplot2::theme_minimal() +
+    ggplot2::labs(y = "Heterogeneity (Q)",
+                  x = axis)
 }
