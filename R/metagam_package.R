d6fd0020842944447596a598c9f7663f3dbf122b
Øystein Sørensen <oyss@medisin.uio.no>
2020-06-22 18:12:09 +0200

    Removed links to rlang

--- R/metagam_package.R
+++ R/metagam_package.R
@@ -17,6 +17,10 @@
 #' @docType package
 #' @name metagam-package
 #'
+#' @importFrom rlang expr enquo enquos sym syms .data := as_name as_label
+#' @aliases expr enquo enquos sym syms .data := as_name as_label
+#' @export expr enquo enquos sym syms .data := as_name as_label
+#'
 NULL
 
 
