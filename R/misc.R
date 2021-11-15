1d0b5285d159aa87d53f34f20b8f06d64abbae37
Øystein Sørensen <oyss@medisin.uio.no>
2021-11-15 15:37:47 +0100

    a lot of refactoring. tests will fail now

--- R/misc.R
+++ R/misc.R
@@ -3,68 +3,184 @@
 #' This code is based on Dr. Gavin Simpson's blog post https://fromthebottomoftheheap.net/2016/12/15/simultaneous-interval-revisited/.
 #'
 #' @param mod Object of class "striprawdata", returned from \code{\link{strip_rawdata}}.
-#' @param grid Grid of values over which to create a simultaneous confidence interval.
-#' @param nmc Number of Monte Carlo samples.
-#' @param terms Character vector of smooth terms.
-#' @param intercept_se Logical defining whether to include the squared standard deviation of the intercept in the
-#' covariance matrix for the smooth term. Defaults to \code{FALSE}.
+#' @param newdat Grid of values over which to create a simultaneous confidence interval.
+#' @param nsim Number of Monte Carlo samples.
+#' @param term Character vector of smooth terms.
 #'
 #' @return A vector of maxima of absolute standard deviations.
 #' @export
 #' @keywords internal
 #'
-getmasd <- function(mod, grid, nmc, terms, intercept_se = FALSE){
+getmasd <- function(mod, newdat, nsim, term){
   Vb <- stats::vcov(mod)
-  inds <- grep(gsub(")", "\\)", gsub("(", "\\(", terms, fixed = TRUE), fixed = TRUE), colnames(Vb))
-  if(intercept_se) inds <- c(inds, grep("Intercept", colnames(Vb)))
+  inds <- grep(gsub(")", "\\)", gsub("(", "\\(", term, fixed = TRUE), fixed = TRUE), colnames(Vb))
+
   Vb <- Vb[inds, inds]
-  pred <- stats::predict(mod, grid, se.fit = TRUE, terms = terms, type = "iterms")
+  pred <- stats::predict(mod, newdat, se.fit = TRUE, terms = term, type = "iterms")
   se.fit <- pred$se.fit
 
-  BUdiff <- mgcv::rmvn(nmc, mu = rep(0, nrow(Vb)), V = Vb)
-  Cg <- stats::predict(mod, grid, type = "lpmatrix")[, inds]
+  BUdiff <- mgcv::rmvn(nsim, mu = rep(0, nrow(Vb)), V = Vb)
+  Cg <- stats::predict(mod, newdat, type = "lpmatrix")[, inds]
   simDev <- Cg %*% t(BUdiff)
   absDev <- abs(sweep(simDev, 1, se.fit, FUN = "/"))
   apply(absDev, 2L, max)
 
 }
 
+# Find the terms from each model
+find_terms <- function(models, type, terms){
+  unique(lapply(models, function(model) {
+    if(type == "iterms"){
+      if(!is.null(terms)){
+        model_terms <- model$term_list[names(model$term_list) %in% terms]
+        if(!all(check <- terms %in% names(model_terms))){
+          stop(paste("Unknown term", names(model_terms)[!check], "requested"))
+        }
+      } else {
+        model_terms <- model$term_list[sort(names(model$term_list))[[1]]]
+        if(length(model_terms) == 0){
+          stop("No terms")
+        }
+      }
+    } else {
+      model_terms <- model$term_list
+    }
 
-
-get_sim_ci <- function(m, cohort_estimates, xvars, method, grid){
-  dd <- merge(cohort_estimates, m, by = "model")
-  dd$weight <- dd$se * dd$crit
-  dd <- eval(parse(text = paste0("split(dd, f = list(dd$", xvars, ", dd$term))")))
-  res <- do.call(rbind, lapply(dd, function(x){
-    m <- metafor::rma(yi = x$estimate, sei = x$weight, method = method)
-    dd3 <- as.data.frame(stats::predict(m))[, c("pred", "se")]
-    dd3$ci_sim_lb <- dd3$pred - dd3$se
-    dd3$ci_sim_ub <- dd3$pred + dd3$se
-    dd3$se <- NULL
-    dd3
-  }))
-  eval(parse(text = paste0("res$", xvars, "<- grid$", xvars)))
-
-  res
+    data.frame(
+      xvars = unlist(model_terms), terms = names(model_terms)
+    )
+  }))[[1]]
 }
 
 
-get_meta_sim_ci <- function(models, ci_alpha, masd_list,
-                            cohort_estimates, xvars, method, grid){
-  crit <- do.call(rbind, lapply(seq_along(models), get_crit_models, ci_alpha, masd_list))
-  get_sim_ci(crit, cohort_estimates, xvars, method, grid)
+create_grid <- function(models, term_list, grid_size){
+  xvars <- unlist(unique(lapply(models, function(model) names(model$var.summary))))
+  ranges <- lapply(models, function(model){
+    ret <- lapply(xvars, function(xvar){
+      v <- model$var.summary[[xvar]]
+      if(is.numeric(v)){
+        range(v)
+      } else {
+        v[[1]]
+      }
+    })
+    names(ret) <- xvars
+    ret
+  })
+  grid <- lapply(xvars, function(xvar){
+    val <- unlist(lapply(ranges, function(range) range[[xvar]]))
+    if(xvar %in% term_list$xvars){
+      seq(from = min(val), to = max(val), length.out = grid_size)
+    } else if(is.numeric(val)){
+      min(val)
+    } else {
+      val[[1]]
+    }
+  })
+  names(grid) <- xvars
+  grid
 }
 
-get_crit <- function(a, ind, masd_list){
-  stats::quantile(masd_list[[ind]], probs = 1 - a, type = 8, names = FALSE)
+
+extract_model_fits <- function(model, term_list, grid){
+  if(type == "iterms"){
+    Map(function(term, xvar){
+      newdat <- create_newdat(xvar = xvar, grid = grid, type = type)
+
+      pred <- stats::predict(model, newdata = newdat, type = type,
+                             se.fit = TRUE, terms = term)
+      if(intercept) pred$fit <- pred$fit + attr(pred, "constant")
+      newdat$fit <- as.numeric(pred$fit)
+      newdat$se.fit <- as.numeric(pred$se.fit)
+      newdat
+    },
+    term = term_list$terms,
+    xvar = term_list$xvars)
+  } else if(type %in% c("link", "response")){
+    newdat <- create_newdat(term_list = term_list, grid = grid, type = type)
+    pred <- stats::predict(model, newdata = newdat, type = type, se.fit = TRUE)
+    newdat$fit <- pred$fit
+    newdat$se.fit <- pred$se.fit
+    newdat
   }
-
-get_crit_models <- function(ind, ci_alpha, masd_list){
-  data.frame(crit = get_crit(ci_alpha, ind, masd_list), alpha = ci_alpha, model = ind)
 }
 
-testfun <- function(a, models, masd_list, cohort_estimates, xvars, method, grid){
-  sim_ci <- get_meta_sim_ci(models, a, masd_list,
-                            cohort_estimates, xvars, method, grid)
-  max(sim_ci$ci_sim_lb) - min(sim_ci$ci_sim_ub)
+fit_meta_models <- function(fits, se.fits, method){
+  fits <- do.call(cbind, fits)
+  se.fits <- do.call(cbind, se.fits)
+  fits <- split(fits, seq_len(nrow(fits)))
+  se.fits <- split(se.fits, seq_len(nrow(se.fits)))
+
+  Map(function(fit, se.fit){
+    metafor::rma(yi = fit, sei = se.fit, method = method)
+  }, fit = fits, se.fit = se.fits)
+}
+
+get_predictions <- function(meta_models){
+  ret <- lapply(meta_models, function(x){
+    pred <- stats::predict(x)
+    data.frame(
+      estimate = pred$pred,
+      se = pred$se
+    )
+  })
+  do.call(rbind, ret)
+}
+
+create_newdat <- function(xvar = NULL, term_list = NULL, grid, type){
+  if(type == "iterms"){
+    newdat <- lapply(grid[!names(grid) %in% xvar], function(x) x[[1]])
+    eval(parse(text = paste0("newdat$", xvar, "<- grid$", xvar)))
+    as.data.frame(newdat)
+  } else {
+    newdat <- lapply(grid[!names(grid) %in% term_list$xvars], function(x) x[[1]])
+    for(xvar in term_list$xvars) eval(parse(text = paste0("newdat$", xvar, "<- grid$", xvar)))
+    expand.grid(newdat)
+  }
+}
+
+get_sim_ci <- function(models, cohort_estimates, masd_list,
+                       term, xvar, ci_alpha){
+  sim_cohort_estimates <- Map(function(model, cohort_estimate, masd){
+    cohort_estimate$crit.width <- quantile(masd, probs = 1 - ci_alpha, type = 8) * cohort_estimate$se.fit
+    cohort_estimate
+  },
+  model = models,
+  cohort_estimate = lapply(cohort_estimates, function(x) x[[term]]),
+  masd = masd_list
+  )
+
+  sim_ci_models <- fit_meta_models(lapply(sim_cohort_estimates, function(x) x[["fit"]]),
+                                   lapply(sim_cohort_estimates, function(x) x[["crit.width"]]),
+                                   method)
+  cbind(
+    create_newdat(xvar = xvar, grid = grid, type = type),
+    get_predictions(sim_ci_models))
+}
+
+
+simulate <- function(term_list, grid, models, nsim, cohort_estimates, ci_alpha){
+  Map(function(term, xvar){
+    newdat <- create_newdat(xvar = xvar, grid = grid, type = "iterms")
+    masd_list <- lapply(models, getmasd, newdat, nsim, term)
+    meta_sim_ci <- get_sim_ci(models, cohort_estimates, masd_list, term, xvar, ci_alpha)
+
+    optfun <- function(sig){
+      dd <- get_sim_ci(models, cohort_estimates, masd_list, term, xvar, sig)
+      max(dd$estimate - dd$se) - min(dd$estimate + dd$se)
+    }
+
+    pval <- if(sign(optfun(1/nsim)) == sign(optfun(.99))){
+      paste("<", 1/nsim)
+    } else {
+      tryCatch(
+        {as.character(stats::uniroot(optfun, interval = c(0, 1))$root)},
+        error = function(e) NA_character_
+      )
+    }
+
+    list(meta_sim_ci = meta_sim_ci, pval = pval)
+  },
+  term = term_list$terms,
+  xvar = term_list$xvars)
 }
---
+++ misc/dev_scripts.R
@@ -0,0 +1,42 @@
+library(metagam)
+library(mgcv)
+
+## Create 5 datasets
+set.seed(1234)
+datasets <- lapply(1:5, function(x) gamSim(scale = 5, verbose = FALSE))
+
+## Fit a GAM in each dataset, then use strip_rawdata() to remove
+## individual participant data
+models <- lapply(datasets, function(dat){
+  ## This uses the gam() function from mgcv
+  dat$grp <- factor(sample(1:3, size = nrow(dat), replace = TRUE))
+  model <- gam(y ~ s(x0, bs = "cr") + s(x1, bs = "cr") + grp, data = dat)
+  ## This uses strip_rawdata() from metagam
+  strip_rawdata(model)
+})
+
+grid = NULL
+grid_size = 30
+type = "iterms"
+terms = c("s(x0)", "s(x1)")
+method = "FE"
+nsim = 100
+ci_alpha = 0.05
+intercept = FALSE
+restrict_range = NULL
+devtools::load_all()
+
+meta_analysis <- metagam(models, terms = c("s(x0)", "s(x1)"), grid_size = 30)
+
+## We can print some information
+summary(meta_analysis)
+
+## We can plot the meta-analytic fit together with the individual fits
+plot(meta_analysis)
+
+## We can also compute p-values and simultaneous confidence intervals, by setting the nsim argument.
+## For details, see the separate vignette.
+\dontrun{
+  meta_analysis <- metagam(models, terms = "s(x0)", grid_size = 30, nsim = 1000)
+  summary(meta_analysis)
+}
